93ed92af7211828443c2933c92717c22121d19a2
astralwaters <markobattlenet@gmail.com>
2023-07-14 17:58:46 +0200

    Added Temp Data

--- VelvetWeb/Controllers/CategoryController.cs
+++ VelvetWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace VelvetWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -72,7 +73,8 @@ namespace VelvetWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
             }
             return View(obj);
         }
@@ -107,7 +109,8 @@ namespace VelvetWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
 
             
         }
