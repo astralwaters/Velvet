5a0a445cef66115afb0879570272bdc62f1214c3
astralwaters <markobattlenet@gmail.com>
2023-07-19 16:40:09 +0200

    Added : Category Repository

---
+++ Velvet.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velvet.DataAccess.Repository.IRepository;
+using Velvet.Models;
+using VelvetWeb.DataAccess;
+
+namespace Velvet.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
