e92b9f99f27945fdb5fe4eb6df5215aa6b296b7c
astralwaters <markobattlenet@gmail.com>
2023-07-21 13:32:52 +0200

    Added Product Model, Table to Db

--- Velvet.DataAccess/Migrations/20230721113143_addProductToDb.Designer.cs
+++ Velvet.DataAccess/Migrations/20230721113143_addProductToDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VelvetWeb.DataAccess;
 
@@ -10,9 +11,11 @@ using VelvetWeb.DataAccess;
 namespace VelvetWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230721113143_addProductToDb")]
+    partial class addProductToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Velvet.Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velvet.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        [Required]
+        [Range(1,10000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+        [Required]        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public int CoverTypeId { get; set; }
+        public CoverType CoverType { get; set; }
+
+    }
+}
