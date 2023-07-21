445136089967eddeae778b4c2d73704fac732a63
astralwaters <markobattlenet@gmail.com>
2023-07-21 13:48:48 +0200

    Product Repository

---
+++ Velvet.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velvet.DataAccess.Repository.IRepository;
+using Velvet.Models;
+using VelvetWeb.DataAccess;
+
+namespace Velvet.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.ListPrice= obj.ListPrice;
+                objFromDb.Price = obj.Price;
+                objFromDb.CategoryId= obj.CategoryId;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+
+            }
+        }
+    }
+}
