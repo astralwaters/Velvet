50ff324eb76ff94543fd4e95c28ee33154335693
astralwaters <markobattlenet@gmail.com>
2023-07-28 17:33:21 +0200

    Working on image upload, and creating first product

--- Velvet.Models/Product.cs
+++ Velvet.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -21,13 +22,16 @@ namespace Velvet.Models
         [Required]
         [Range(1, 10000)]
         public double Price { get; set; }
-
+        [ValidateNever]
         public string ImageUrl { get; set; }
         [Required]        
         public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
         [Required]
         public int CoverTypeId { get; set; }
+        [ValidateNever]
         public CoverType CoverType { get; set; }
 
     }
